27cd26828c502c27c6d22d46b55a500da4ac2026
Kyungmin, Lee (Kevin) <53737958+klee30810@users.noreply.github.com>
2021-01-30 16:29:02 +0900

    Add files via upload

--- algorithm/DataStructure/6. Stack/ListBaseStack/ListBaseStackMain.c
+++ algorithm/DataStructure/6. Stack/ListBaseStack/ListBaseStackMain.c
@@ -8,13 +8,15 @@ int main(void)
 	StackInit(&stack);
 
 	// 데이터 넣기 ///////
-	SPush(&stack, 1);  SPush(&stack, 2);
-	SPush(&stack, 3);  SPush(&stack, 4);
+	SPush(&stack, 1);
+	SPush(&stack, 2);
+	SPush(&stack, 3);
+	SPush(&stack, 4);
 	SPush(&stack, 5);
 
 	// 데이터 꺼내기 ///////
-	while(!SIsEmpty(&stack))
-		printf("%d ", SPop(&stack)); 
+	while (!SIsEmpty(&stack))
+		printf("%d ", SPop(&stack));
 
 	return 0;
 }
