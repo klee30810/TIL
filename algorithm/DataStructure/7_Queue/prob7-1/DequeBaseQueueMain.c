d98f366dccd50a641e61a7f02c0a1050350b20e5
klee30810 <klee30810@gmail.com>
2021-03-14 14:36:49 +0900

    ds prob7-1 upload

---
+++ algorithm/DataStructure/7_Queue/prob7-1/DequeBaseQueueMain.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "DequeBaseQueue.h"
+
+int main()
+{
+    // Queue Initialization
+    Queue q;
+    QueueInit(&q);
+
+    // Queue Insert
+    Enqueue(&q, 1);
+    Enqueue(&q, 2);
+    Enqueue(&q, 3);
+    Enqueue(&q, 4);
+    Enqueue(&q, 5);
+
+    while(!QIsEmpty(&q))
+        printf("%d ", Dequeue(&q));
+
+    return 0;
+}
